8ee7a191df611c92ef6a85c704c1e9d4b42a20da
Samwit <samwitadhikary2@gmail.com>
2021-02-12 19:19:40 +0530

    Factorial of a number

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, i, f = 1;
+        System.out.println("Enter any number: ");
+        a = scan.nextInt();
+        for (i = 1; i <= a; i++) {
+            f = f * i;
+            scan.close();
+        }
+        System.out.print(f);
+    }
+}
